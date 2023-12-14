02a73e76177b9b13bc104f449a591e9e6aa57937
Eelavan <80886262+Maskoff9@users.noreply.github.com>
2023-12-14 21:48:55 +0100

    Add files via upload

--- TME8/src/ServerSocket.h
+++ TME8/src/ServerSocket.h
@@ -10,12 +10,42 @@ class ServerSocket {
 
 public :
 	// Demarre l'ecoute sur le port donne
-	ServerSocket(int port);
+	ServerSocket(int port)ServerSocket(int port){
+		socketfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (socketfd <0){
+			perror ("problème socket");
+		}
+		struct sockaddr_in adr;
+		adr.sin_family = AF_INET;
+		adr.sin_port = htons(port);
+		adr.sin_addr.s_addr = INADDR_ANY ;
+		std::cout << "Coucou je suis un serveur" << std::endl;
+		if (bind (socketfd, (struct sockaddr *) &adr, sizeof adr) < 0){
+			perror ("bind");
+		}
+		if (listen(socketfd, 10)<0){
+			perror ("listen");
+		}
+	
 
 	int getFD() { return socketfd;}
 	bool isOpen() const {return socketfd != -1;}
 
-	Socket accept();
+	Socket accept(){
+		struct sockaddr exp;
+		socklen_t sz;
+		int sc = ::accept(socketfd, &exp, &sz);	// On reçoit la taille !
+		struct sockaddr_in expIn;
+		std::cout << "connexion de "<< & expIn;
+		return Socket(sc);
+	}
+
+	void close(){
+		if (socketfd != -1){
+			::close(socketfd);
+			socketfd = -1;
+		}
+	}
 
 	void close();
 };
